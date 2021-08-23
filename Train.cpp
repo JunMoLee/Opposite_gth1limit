bbc835690bc81481c7e79f06a3685504d0815993
JunMoLee <59989788+JunMoLee@users.noreply.github.com>
2021-08-24 04:12:49 +0900

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -1459,7 +1459,7 @@ double s2[param->nOutput];  // Output delta from hidden layer to the output laye
 				
 
 						for (int k = 0; k < param->nHide; k++) {
-						printf("noisypulse: %.5f, realpulse: %.5f, noise: %.5f, conductanceGp: %.5f, conductanceGn: %.5f, s2[%d]: %.5f, a2[%d]: %.5f, conductancePrev: %.5f \n",static_cast<RealDevice*>(arrayHO->cell[j][k])->noisypulse,static_cast<RealDevice*>(arrayHO->cell[j][k])->realpulse, static_cast<RealDevice*>(arrayHO->cell[j][k])->noise,static_cast<RealDevice*>(arrayHO->cell[j][k])->conductanceGp,static_cast<RealDevice*>(arrayHO->cell[j][k])->conductanceGn,j, s1[j],j,a1[j],static_cast<RealDevice*>(arrayHO->cell[j][k])->conductancePrev );}
+						printf("noisypulse: %.5f, realpulse: %.5f, noise: %.5f, conductanceGp: %.5f, conductanceGn: %.5f, s2[%d]: %.5f, a2[%d]: %.5f, conductancePrev: %.5f \n",static_cast<RealDevice*>(arrayHO->cell[j][k])->noisypulse,static_cast<RealDevice*>(arrayHO->cell[j][k])->realpulse, static_cast<RealDevice*>(arrayHO->cell[j][k])->noise,static_cast<RealDevice*>(arrayHO->cell[j][k])->conductanceGp,static_cast<RealDevice*>(arrayHO->cell[j][k])->conductanceGn,j, s2[j],j,a2[j],static_cast<RealDevice*>(arrayHO->cell[j][k])->conductancePrev );}
 					
 					}
 				}
